26c0a6a2ca01647190c2e6b4c5b15115c6e82b80
entity <entity@voodoo.systems>
2019-12-15 15:43:49 +0100

    some extensions

--- src/Api.php
+++ src/Api.php
@@ -7,6 +7,13 @@ class Api {
     public function __construct() {
         $this->client = Connection::getGuzzleClient();
     }
+
+    public function getEntityTypes() {
+        $path = "getEntityTypes";
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
     
     public function getEntityByTypeAndId($type,$id,$traverse = false)  {
         // build the request string
@@ -22,7 +29,35 @@ class Api {
     public function getEntitiesByType($type) {
         // build the request string
         $path = "getEntitiesByType?type=" . $type;
-        $ret = $this->client->request("GET",$path);
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
+
+    public function getEntitiesByTypeAndValue($type,$value) {
+        $path = "getEntitiesByTypeAndValue?type=" . $type . "&value=" . $value;
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
+
+    public function getEntitiesByValue($value) {
+        $path = "getEntitiesByValue?value=" . $value;
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
+
+    public function getParentEntities($type,$id) {
+        $path = "getParentEntities?type=" . $type . "&id=" . $id;
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
+
+    public function getChildEntities($type,$id) {
+        $path = "getChildEntities?type=" . $type . "&id=" . $id;
+        $ret  = $this->client->request("GET",$path);
         $data = $this->parseReturn($ret);
         return $data;
     }
@@ -66,7 +101,18 @@ class Api {
         $ret = $this->client->request("DELETE", $path);
         $this->parseReturn($ret);
     }
+
+    public function mapJson($json) {
+        $ret = $this->client->request("POST", "createEntity", [
+           "body" => $json
+        ]);
+        $data = $this->parseReturn($ret);
+    }
+
+    public function getRelation($srcType,$srcID,$targetType,$targetID) {
+        $path = "getRelation?srcType=" . $srcType . "&srcID=" . $srcID . "&targetType=" . $targetType . "&targetID=" . $targetID;
+        $ret  = $this->client->request("GET",$path);
+        $data = $this->parseReturn($ret);
+        return $data;
+    }
 }
-
-
-?>
